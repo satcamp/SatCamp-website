56df54346d64c827ed88aa1fd87318d88b018a3d
Ian Schuler <ian@developmentseed.org>
2023-08-31 10:56:39 -0400

    Update agenda.tsx
    A few content changes / clarifications:
    - clarified some start and end times to be more useful for locals (no content until 3 on tues; group dinner on tues; end time on weds)
    - apparently we aren't providing box lunch on Thursday

--- src/components/agenda.tsx
+++ src/components/agenda.tsx
@@ -11,9 +11,9 @@ export function AgendaSection() {
         <SubSectionTitle>Tuesday September 12th, 2023</SubSectionTitle>
         <List>
           <Item>11:30am Arrival at Chautauqua Lodge</Item>
-          <Item>12:00pm Start of content</Item>
-          <Item>NCAR hike & tour</Item>
-          <Item>Welcome talks</Item>
+          <Item>12:00pm NCAR hike & tour</Item>
+          <Item>3:00pm Welcome talks and scene setting</Item>
+          <Item>6:00-8:00pm Dinner and "campfire" activities</Item>
         </List>
         <SubSectionTitle>Wednesday September 13th, 2023</SubSectionTitle>
         <List>
@@ -22,14 +22,13 @@ export function AgendaSection() {
           <Item>Morning presentations and discussion</Item>
           <Item>Lunch provided in to-go boxes for activities</Item>
           <Item>Outdoor activities (hiking, biking, coffee/brewery, bouldering)</Item>
-          <Item>Activity debrief, afternoon presentations and discussion</Item>
+          <Item>2:30-5:00 Afternoon presentations and discussion</Item>
         </List>
         <SubSectionTitle>Thursday September 14th, 2023</SubSectionTitle>
         <List>
           <Item>8:00am Breakfast</Item>
           <Item>8:30am Start of content</Item>
           <Item>Morning presentations and discussion</Item>
-          <Item>Lunch provided in to-go boxes for activities</Item>
           <Item>Outdoor activities (hiking, biking, coffee/brewery, bouldering)</Item>
           <Item>Recap & reflections</Item>
           <Item>4:00pm Adjourn</Item>
