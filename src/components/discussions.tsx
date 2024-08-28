6813b9bc7de948c1a893fc00c041fc98be6118a5
Tyler Erickson <tyler@vorgeo.com>
2024-08-28 10:49:10 -0700

    Remove climate section

--- src/components/discussions.tsx
+++ src/components/discussions.tsx
@@ -18,21 +18,6 @@ export function DiscussionsSection() {
           What’s the right way to fund a new satellite that serves a public good?
           Should each sector have their own satellite fleet?
         </p>
-        <SubSectionTitle image_name="forest">Climate</SubSectionTitle>
-        <p>
-          With global{" "}
-          <a
-            className="underline underline-offset-2"
-            href="https://climate.copernicus.eu/climate-indicators"
-            target="_blank"
-          >
-            climate indicators
-          </a>{" "}
-          blaring alarms, what can the data and analytics community do to impact
-          the societal conversation? Will current market based approaches be
-          fast enough to move the needle on a global scale? What are viable
-          alternatives given political realities?
-        </p>
         <SubSectionTitle image_name="chip">
           GeoAI and Remote Sensing
         </SubSectionTitle>
