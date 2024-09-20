6971a2c35bb2de41ca1cd791776927482f1e8080
Tyler Erickson <tyler@vorgeo.com>
2024-09-20 13:53:51 -0700

    Updates website to match working agenda

--- src/components/agenda.tsx
+++ src/components/agenda.tsx
@@ -25,10 +25,10 @@ export function AgendaSection() {
           <Item>11:30am Arrival at Chautauqua Lodge</Item>
           <List>
             <Item>Option 1: NOAA hike & tour (pre-registration required)</Item>
-            <Item>Option 2: Outdoor small group activities</Item>
+            <Item>Option 2: Acclimatization activities (hikes, games)</Item>
           </List>
-          <Item>3:15pm Welcome talks and scene setting</Item>
-          <Item>4:00pm Panel talks</Item>
+          <Item>3:15pm Welcome talk, scene setting & activities</Item>
+          <Item>4:00pm Small group activities</Item>
           <Item>5:00pm Unstructured time / travel to downtown Boulder</Item>
           <Item>6:30pm Happy Hour at <a href="https://boulder.avantifandb.com/">Avanti Rooftop</a></Item>
         </List>
@@ -36,23 +36,25 @@ export function AgendaSection() {
         <List>
           <LocationDesc>Grand Assembly Hall</LocationDesc>
           <Item>8:00am Breakfast</Item>
-          <Item>9:00am-12:00pm Outdoor activities</Item>
+          <Item>9:00am Outdoor activities</Item>
           <Item>12:00pm Lunch</Item>
           <Item>1:00pm Lightning talks (part 1)</Item>
-          <Item>2:00pm Panel talks</Item>
-          <Item>4:00pm Small group discussions</Item>
+          <Item>2:00pm Panel talk: GeoAI</Item>
+          <Item>3:00pm Small group activities</Item>
+          <Item>4:00pm Panel talk: Satellite Business Models</Item>
           <Item>6:30pm Small group dinners (self-organized)</Item>
         </List>
         <SubSectionTitle>Thursday October 3rd, 2024</SubSectionTitle>
         <List>
           <LocationDesc>Grand Assembly Hall</LocationDesc>
           <Item>8:00am Breakfast</Item>
-          <Item>9:00am Panel talks</Item>
-          <Item>10:00am Outdoor activities (w/ grab & go lunch)</Item>
+          <Item>9:00am Panel talk: Geo Platforms as a Business</Item>
+          <Item>10:00am Cabin check out</Item>
+          <Item>10:30am Outdoor activities (w/ grab & go lunch)</Item>
           <Item>1:00pm Lightning talks (part 2)</Item>
-          <Item>2:00pm Small group discussions</Item>
-          <Item>3:00pm Closing</Item>
-          <Item>4:00pm onwards Optional happy hour</Item>
+          <Item>2:00pm Snack time & small group time</Item>
+          <Item>3:00pm Closing session</Item>
+          <Item>4:00pm+ Optional happy hour (@ <a href="https://www.therayback.com/">Rayback Collective</a>)</Item>
         </List>
       </RightColumn>
     </Section>
